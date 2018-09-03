1b753e033d0fad18d7ae6a913fe41ba4bf18e713
Harshil <harshil.dilipcumar@itv.com>
2018-09-03 10:06:21 +0100

    code refactor

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,13 +9,13 @@ public class App
     private final String message = "Hello World!";
 
     public App() {}
+    
+    private final String getMessage() {
+        return message;
+    }
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-    }
-
-    private final String getMessage() {
-        return message;
     }
 
 }
